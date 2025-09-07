1b7df3607db6c9cd74c7163df11caa157cebbb94
배건우 <102368729+mclusky0626@users.noreply.github.com>
2025-09-07 17:03:54 +0900

    config
    dd

--- musicer/vite.config.js
+++ musicer/vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // 👇 이 server 설정을 추가하거나 기존 server 설정에 headers를 추가하세요.
-  server: {
-    headers: {
-      'Cross-Origin-Opener-Policy': 'same-origin-allow-popups',
-    },
-  },
 })
